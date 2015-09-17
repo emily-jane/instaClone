e68fe004378f5d6cf414b6f595d126e6862adb04
Emily Carson <emilyjanecarson@gmail.com>
2015-09-17 16:54:21 +0100

    ten passing tests, name field is required

--- Gemfile
+++ Gemfile
@@ -52,5 +52,6 @@ end
 group :test do
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'rspec-collection_matchers'
 end
 
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe Image, type: :model do
   it { is_expected.to have_many :comments }
+
+  it 'is not valid when no name is added' do
+    image = Image.new(name: '')
+    expect(image).to have(1).error_on(:name)
+    expect(image).not_to be_valid
+  end
 end
