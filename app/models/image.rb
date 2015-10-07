1f21074072f6ff1b33f5a3ee10dd15925fadcab3
Emily Carson <emilyjanecarson@gmail.com>
2015-10-07 20:30:16 +0100

    added more foundation, pictures not stretching to grid size

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "480x480>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :comments, dependent: :destroy
   validates :name, length: {minimum: 1}
